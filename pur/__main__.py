9cd724316e96ea635197b044f4f5438ea0843e2d
Alan Hamlett <alan.hamlett@gmail.com>
2019-04-10 09:17:40 -0700

    add __main__.py for module importing

---
+++ pur/__main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    pur.__main__
+    ~~~~~~~~~~~~
+    Update packages in a requirements.txt file to latest versions.
+    :copyright: (c) 2016 Alan Hamlett.
+    :license: BSD, see LICENSE for more details.
+"""
+
+
+from .__init__ import pur
+
+
+if __name__ == '__main__':
+    pur()
